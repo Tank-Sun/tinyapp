22daab34cd4eadc252e3c67d44ef0d089c59fc50
Yongjia Sun <thomastank0926@gmail.com>
2022-10-05 03:55:15 +0000

    imply the sign in operation using cookie

--- express_server.js
+++ express_server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 function generateRandomString() {
   let result = '';
@@ -22,16 +24,18 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.render("homepage");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("homepage", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -41,13 +45,18 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${id}`); // Redirect to the /urls/:id page
 });
 
+app.post("/urls/signin", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { username: req.cookies["username"], id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
